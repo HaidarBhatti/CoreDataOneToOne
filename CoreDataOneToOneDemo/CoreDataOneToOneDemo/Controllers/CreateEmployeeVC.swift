4694f4a24b9367701116b8c5fb8ab4aff5ad3ded
haidar bhatti <haidarbhatti.hb85@gmail.com>
2023-02-25 21:55:56 +0500

    CreateEmployeeVC is added

---
+++ CoreDataOneToOneDemo/CoreDataOneToOneDemo/Controllers/CreateEmployeeVC.swift
@@ -0,0 +1,31 @@
+//
+//  CreateEmployeeVC.swift
+//  CoreDataOneToOneDemo
+//
+//  Created by Haidar Bhatti on 24/02/2023.
+//
+
+import UIKit
+
+class CreateEmployeeVC: UIViewController {
+
+    @IBOutlet weak var imgEmployee: UIImageView!
+    @IBOutlet weak var textName: UITextField!
+    @IBOutlet weak var textEmail: UITextField!
+    @IBOutlet weak var textPassportNo: UITextField!
+    @IBOutlet weak var textPassportPlaceOfIssue: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onTapSave(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func onTapDeleteAll(_ sender: UIButton) {
+    
+    }
+    
+}
+
