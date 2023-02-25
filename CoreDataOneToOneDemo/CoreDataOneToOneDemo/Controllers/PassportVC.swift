1f75f756b671fc6fb98e098bb91f7fe55359eae3
haidar bhatti <haidarbhatti.hb85@gmail.com>
2023-02-25 21:56:13 +0500

    PassportVC is added

---
+++ CoreDataOneToOneDemo/CoreDataOneToOneDemo/Controllers/PassportVC.swift
@@ -0,0 +1,19 @@
+//
+//  PassportVC.swift
+//  CoreDataOneToOneDemo
+//
+//  Created by Haidar Bhatti on 24/02/2023.
+//
+
+import UIKit
+
+class PassportVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
