a6019ebeaa1896ae6e570bacff261db1fe7fed88
haidar bhatti <haidarbhatti.hb85@gmail.com>
2023-02-25 21:56:05 +0500

    EmployeeListVC is added

---
+++ CoreDataOneToOneDemo/CoreDataOneToOneDemo/Controllers/EmployeeListVC.swift
@@ -0,0 +1,19 @@
+//
+//  EmployeeListVC.swift
+//  CoreDataOneToOneDemo
+//
+//  Created by Haidar Bhatti on 24/02/2023.
+//
+
+import UIKit
+
+class EmployeeListVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
